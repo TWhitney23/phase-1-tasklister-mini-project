7e06e6c6cf56b4a967f47a09ee362e1377e60945
Travelle Whitney <travellewhitney@gmail.com>
2025-08-13 16:53:04 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#create-task-form");
+  const taskInput = document.querySelector("#new-task-description");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+      const li = document.createElement("li");
+      li.textContent = taskText;
+      taskList.appendChild(li);
+
+      taskInput.value = "";
+    }
+  });
+});
